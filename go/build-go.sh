e52cc5bf2031d484d5014a1d413953195852c632
Will Dollman <will.dollman@sourcegraph.com>
2024-11-12 13:13:45 +0000

    Minimise go binaries

--- go/build-go.sh
+++ go/build-go.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-GOOS=linux GOARCH=amd64 go build -o bin/go-example ../go/cmd/example/main.go
-GOOS=linux GOARCH=amd64 go build -o bin/go-example-network ../go/cmd/example-network/main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-s -w" -o bin/go-example ../go/cmd/example/main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-s -w" -o bin/go-example-network ../go/cmd/example-network/main.go
